317adc71e77168ba367d9e7ae163c14c9025419b
Corey Krewson <ckrewson@aquaveo.com>
2023-12-27 20:12:59 +0000

    tag version v0.0.1_0_2023_12_27

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_resource_files
+from setup_helper import find_all_resource_files
 
 # -- Apps Definition -- #
-app_package = 'bokeh_tutorial'
-release_package = 'tethysapp-' + app_package
+namespace = 'tethysapp'
+app_package = "bokeh_tutorial"
+release_package = "tethysapp-" + app_package
 
 # -- Python Dependencies -- #
 dependencies = []
 
 # -- Get Resource File -- #
-resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
+resource_files = find_all_resource_files(app_package, namespace)
 
 
 setup(
     name=release_package,
-    version='0.0.1',
-    description='',
-    long_description='',
-    keywords='',
-    author='',
-    author_email='',
-    url='',
-    license='',
+    version="0.0.1",
+    description="",
+    long_description="",
+    keywords="replace_keywords",
+    author="",
+    author_email="_email",
+    url="",
+    license="",
     packages=find_namespace_packages(),
-    package_data={'': resource_files},
+    package_data={"": resource_files},
     include_package_data=True,
     zip_safe=False,
     install_requires=dependencies,
